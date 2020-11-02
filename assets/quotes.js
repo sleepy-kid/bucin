f173f38366eef37b4fc1f65c1df85bf6dc26be43
sleepy-kid <73792359+sleepy-kid@users.noreply.github.com>
2020-11-02 11:30:23 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! hilda" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you hilda ." },
+    { author: "utuk kamu", quotes: "Whatever happens, keep breathing" },
+    { author: "utuk kamu", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "utuk kamu", quotes: "Hey! hilda" },
+    { author: "utuk kamu", quotes: "I love you." },
+    { author: "utuk kamu", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "utuk kamu", quotes: "I love you." },
+    { author: "utuk kamu", quotes: "I'm sorry for loving you hilda ." },
 ];
